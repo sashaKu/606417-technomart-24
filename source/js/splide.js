129187367491aad7fb6b43a16970f4d37b5f0d38
Sasha <ukemi-atemi@ya.ru>
2021-02-27 02:36:08 +0300

    added and set slider

---
+++ source/js/splide.js
@@ -0,0 +1,12 @@
+new Splide( '.splide', {
+
+	classes: {
+
+		arrows: 'slider__nav-arrows',
+		prev: 'slider__nav-prev ',
+		next: 'slider__nav-next',
+		pagination: 'slider__marker-list',
+		page: 'slider__marker-item'
+	}
+
+}).mount();
